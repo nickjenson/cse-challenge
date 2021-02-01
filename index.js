fc2ef909c7428ed43013e0e69dfc4be91b30f853
Nicholas Jenson <njenson@instructure.com>
2021-01-31 17:12:11 -0800

    This was primarily updates to defined variables and formatting.

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ const axios = require('axios');
 axios.defaults.baseURL = 'https://stucse.kuali.co/';
 axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.TOKEN}`;
 
-
-(async () => {
-    const data = await getAllData();
-    fs.createReadStream('data/courses.csv')
-        .pipe(csv())
-        .on('data', row => {
-            postData(row, data);
-        })
-        .on('end', () => {
-            console.log('complete');
-        });
-})();
-
 const getAllData = async () => {
     try {
         let data = [];
@@ -36,44 +23,42 @@ const getData = async url => {
 };
 
 const postData = (row, data) => {
-    let creditType = row.creditType;
-    let start = row.dateStart.split(" ");
-    let season = start[0].toLowerCase();
-    let year = start[1];
-    let min = row.creditsMin;
-    let max = row.creditsMax;
-    let campuses = row.campus.split(",");
-    let creditDetails = ''
-    let value = ''
+    const creditType = row.creditType,
+        min = row.creditsMin,
+        max = row.creditsMax,
+        start = row.dateStart.split(' '),
+        season = start[0].toLowerCase(),
+        year = start[1],
+        campuses = row.campus.split(',');
+    let creditDetails, value;
 
-    let campus = {};
-
+    const campus = {};
     campuses.forEach(location => {
         let locationId = data.campuses.find(x => x.name === location).id;
         campus[locationId] = true;
-    })
+    });
 
     let date = {
         winter: '01-01',
         spring: '04-03',
         summer: '07-04',
-        fall: '10-04'
-    }
+        fall: '10-04',
+    };
 
     switch (creditType) {
         case 'multiple':
-            creditDetails = { min: min, max: max }
-            value = [ min, max ]
+            creditDetails = { min: min, max: max };
+            value = [min, max];
             break;
 
         case 'range':
-            creditDetails = { min: min, max: max }
-            value = creditDetails
+            creditDetails = { min: min, max: max };
+            value = creditDetails;
             break;
 
         default:
-            creditDetails = { min: min, max: min }
-            value = min
+            creditDetails = { min: min, max: min };
+            value = min;
     }
 
     let postData = {
@@ -90,9 +75,20 @@ const postData = (row, data) => {
         groupFilter1: data.groups.find(x => x.name === row.department)?.id ?? '',
         groupFilter2: data.groups.find(x => x.name === row.department)?.parentId ?? '',
         campus: { ...campus },
-        notes: 'Submitted by Nick Jenson'
+        notes: 'Submitted by Nick Jenson',
     };
-    console.log(postData)
-}
+    console.log(postData);
+};
 
-readCSV();
+
+(async () => {
+    const data = await getAllData();
+    fs.createReadStream('data/courses.csv')
+        .pipe(csv())
+        .on('data', row => {
+            postData(row, data);
+        })
+        .on('end', () => {
+            console.log('complete');
+        });
+})();
